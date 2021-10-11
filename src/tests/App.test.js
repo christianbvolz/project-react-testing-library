c94fb91ca5a363cf8294220d858361f8a5d62021
Christian Volz <christianbvolz@gmail.com>
2021-10-11 01:00:18 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import renderWithRouter from './utilities/renderWithRouter';
+import App from '../App';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.',
+  () => {
+    it('O primeiro link deve possuir o texto Home.', () => {
+      renderWithRouter(<App />);
+      const homeLink = screen.getByRole('link', { name: 'Home' });
+      expect(homeLink).toBeInTheDocument();
+    });
+    it('O segundo link deve possuir o texto About.', () => {
+      renderWithRouter(<App />);
+      const aboutLink = screen.getByRole('link', { name: 'About' });
+      expect(aboutLink).toBeInTheDocument();
+    });
+    it('O terceiro link deve possuir o texto Favorite Pokémons.', () => {
+      renderWithRouter(<App />);
+      const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+      expect(favoriteLink).toBeInTheDocument();
+    });
+  });
---
+++ src/tests/utilities/renderWithRouter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  const rendeObj = render(<Router history={ history }>{component}</Router>);
+  return ({
+    ...rendeObj,
+    history,
+  });
+};
+
+export default renderWithRouter;
